e01a0d386facdd0505950b5720a5db5fdc172cf1
zhinoussh <spring1986@gmail.com>
2016-08-16 12:14:48 +0430

    edit search bar in index file add also jquery ui for enabling autocomplete textbox

---
+++ HotelAdvice/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelAdvice.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Destination_Name { get; set; }
+    }
+}
