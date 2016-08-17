8ebb40672415fe9673053ca7d8a9e0e67ed0545d
zhinoussh <spring1986@gmail.com>
2016-08-17 10:28:41 +0430

    configuration for DB
    add entityFramework
    add Data Access Layer and HotelAdviceDB to App-code folder

---
+++ HotelAdvice/App_Code/DAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelAdvice.App_Code
+{
+    public class DAL
+    {
+    }
+}
---
+++ HotelAdvice/App_Code/HotelAdviceDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace HotelAdvice.App_Code
+{
+    public class HotelAdviceDB : DbContext
+    {
+        public HotelAdviceDB()
+            : base("name=HotelAdviceConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           
+        }
+    }   
+}
