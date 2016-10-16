3e99355c68324ba1066c2d2544d1b8df44c3e288
zhinoussh <spring1986@gmail.com>
2016-10-16 09:54:00 +0330

    set routconfig to search for default area

--- HotelAdvice/App_Start/RouteConfig.cs
+++ HotelAdvice/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace HotelAdvice
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            ).DataTokens = new RouteValueDictionary(new { area = "HomePage" });
         }
     }
 }
