6572d6d7711127529c1a450571e2d2addc618518
zhinoussh <spring1986@gmail.com>
2016-11-30 09:55:19 +0330

    add checkbox and checkbox group extensions

---
+++ HotelAdvice/HtmlExtensions/HtmCheckBoxGroup.cs
@@ -0,0 +1,59 @@
+﻿using HotelAdvice.Areas.Admin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace HotelAdvice
+{
+    public static class HtmCheckBoxGroup
+    {
+        public static MvcHtmlString AwesomeCheckboxGroupFor(this HtmlHelper htmlHelper,
+            string name,List<HotelAmenityViewModel> lst_checkbox, string checkboxCss, Object htmlAttributes = null)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            RouteValueDictionary rvd;
+
+            if (!string.IsNullOrEmpty(checkboxCss))
+                checkboxCss = "checkbox " + checkboxCss;
+            else
+                checkboxCss = "checkbox";
+            string chk_id, chk_status, hd_id_amenity, hd_selected;
+
+            // Add checkboxes
+            for (int i = 0; i < lst_checkbox.Count; i++)
+            {
+                chk_id = "chk_amenity" + lst_checkbox[i].AmenityID;
+                chk_status = lst_checkbox[i].hotel_selected == true ? "true" : "false";
+                hd_id_amenity = name + "[" + i + "].AmenityID";
+                hd_selected = name + "[" + i + "].hotel_selected";
+
+                sb.Append("<div class='checkbox_in_list'>");
+                sb.Append("<div class='" + checkboxCss + "'>");
+                sb.Append("<input type='hidden' name='" + hd_id_amenity + "' value='" + lst_checkbox[i].AmenityID + "' />");
+                
+                rvd = new RouteValueDictionary();
+                HtmlCommon.AddIdName(rvd, hd_selected, chk_id);
+                sb.Append(InputExtensions.CheckBox(htmlHelper, hd_selected,rvd));
+                //   sb.Append("<input type='hidden' name='" + hd_selected + "' value='" + chk_status + "' />");
+                //   sb.Append("<input type='checkbox'  id='" + chk_id +  "' name='" + hd_selected + "' value='true' checked  />");
+               
+                sb.Append("<label for='" + chk_id + "' class='text-small'>");
+                sb.Append(lst_checkbox[i].AmenityName);
+                sb.Append("</label>");
+                sb.Append("</div>");
+                sb.Append("</div>");
+            }
+
+            return MvcHtmlString.Create(sb.ToString());
+
+
+        }
+    }
+}
