d64050f242fdb40c190279280dd5f06a813b462f
zhinoussh <spring1986@gmail.com>
2016-12-12 09:02:19 +0330

    unit testing for admin controller completed

---
+++ HotelAdvice.Tests/Controllers/AdminControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelAdvice.Areas.Admin.Controllers;
+using System.Web.Mvc;
+using Moq;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System.Web;
+using System.Web.Routing;
+
+namespace HotelAdvice.Tests.Controllers
+{
+    [TestClass]
+    public class AdminControllerTests
+    {
+        [TestMethod]
+        public void Index_Return_IndexView()
+        {
+            //Arrange
+            
+            AdminController ctrl = new AdminController();
+            
+            //Act
+            var result = (ViewResult)ctrl.Index();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+       
+    }
+}
--- HotelAdvice/Areas/Admin/Controllers/AdminController.cs
+++ HotelAdvice/Areas/Admin/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using Microsoft.Owin.Security;
+using System.Web;
 using System.Web.Mvc;
 
 namespace HotelAdvice.Areas.Admin.Controllers
@@ -6,16 +7,13 @@ namespace HotelAdvice.Areas.Admin.Controllers
     [Authorize(Roles="Administrator")]
     public class AdminController : Controller
     {
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Logoff()
-        {
-            HttpContext.GetOwinContext().Authentication.SignOut();
-            return RedirectToAction("Index", "Home");
-        }
+       
     }
 }
